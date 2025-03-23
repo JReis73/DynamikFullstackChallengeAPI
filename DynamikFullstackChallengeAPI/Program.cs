c9f91ec4f76e425bc06254adb68120f786a98f4b
José Reis <jose.filreis@gmail.com>
2025-03-23 17:47:54 +0000

    [WIP] Frontend debugging

--- DynamikFullstackChallengeAPI/Program.cs
+++ DynamikFullstackChallengeAPI/Program.cs
@@ -20,6 +20,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
